f8e1d7c37e087c49c7bc5ac34cd030c4eaed98ce
Sebastián Sánchez <sebastian.asm@outlook.com>
2021-12-02 18:11:35 -0300

    renderizando cards de trabajos

--- app.js
+++ app.js
@@ -1,48 +1,58 @@
 (async () => {
   const d = document,
-    date = d.getElementById('date'),
-    enabled = d.getElementById('enabled'),
-    disabled = d.getElementById('disabled'),
-    proyect = d.getElementById('proyect'),
-    dark = d.getElementById('dark');
+    date = d.getElementById('date');
 
-  // header
-  dark.addEventListener('click', () => {
-    console.log('clic!');
-    localStorage.setItem('dark-mode', 'ok');
-  });
+  date.textContent = new Date().getFullYear();
 
-  // main
-  const getData = async () => {
+  async function getData() {
     try {
       const resp = await fetch('./src/db.json'),
         result = await resp.json();
+
       return result;
     } catch {
       return null;
     }
-  };
+  }
 
-  const data = await getData();
+  function renderCards(data) {
+    const card = d.getElementById('template-card').content,
+      fragment = d.createDocumentFragment(),
+      enabled = d.getElementById('enabled'),
+      disabled = d.getElementById('disabled'),
+      proyect = d.getElementById('proyect');
 
-  console.log(data);
+    if (data) {
+      data.forEach((item) => {
+        const { title, description, stack, img, category } = item;
 
-  if (data) {
-    data.forEach((item) => {
-      const { web, img, category } = item;
-      let article = d.createElement('article');
+        card.querySelector('img').src = img;
+        card.querySelector('img').alt = title;
+        card.getElementById('title').textContent = title;
+        card.getElementById('description').textContent = description;
 
-      article.innerHTML = `
-        <p>${web}</p>
-        <img src="${img}" style="width: 120px" />
-      `;
+        renderTechStack(stack, card);
 
-      if (category === 'enabled') enabled.appendChild(article);
-      if (category === 'disabled') disabled.appendChild(article);
-      if (category === 'proyect') proyect.appendChild(article);
+        const clone = card.cloneNode(true);
+        fragment.appendChild(clone);
+
+        if (category === 'enabled') enabled.appendChild(fragment);
+        if (category === 'disabled') disabled.appendChild(fragment);
+        if (category === 'proyect') proyect.appendChild(fragment);
+      });
+    }
+  }
+
+  function renderTechStack(stack, card) {
+    card.getElementById('stack').innerHTML = '';
+
+    stack.forEach((tech) => {
+      const span = d.createElement('span');
+      span.textContent = tech;
+      card.getElementById('stack').append(span);
     });
   }
 
-  // footer
-  date.textContent = new Date().getFullYear();
+  const data = await getData();
+  renderCards(data);
 })();
