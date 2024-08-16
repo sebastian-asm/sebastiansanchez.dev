cafb43d54a32e339175c9ecabf9d3894fb8776e8
Sebastian Sanchez <sasanchezmiles@teco.com.ar>
2024-08-16 12:10:43 -0300

    tomando mensaje desde api

--- js/app.js
+++ js/app.js
@@ -1,2 +1,19 @@
-const year = document.querySelector('small');
-year.textContent = `~ ${new Date().getFullYear()}`;
+import loading from './ui/loading.js'
+
+document.addEventListener('DOMContentLoaded', async () => {
+  const year = document.querySelector('small')
+  const messageContainer = document.querySelector('#message-container')
+  year.textContent = `~ ${new Date().getFullYear()}`
+
+  try {
+    loading(true)
+    const url = 'https://api.npoint.io/9780e5acedfafd28b950'
+    const { message } = await fetch(url).then((data) => data.json())
+    messageContainer.innerHTML = message
+  } catch {
+    messageContainer.textContent = '¡Oops! algo salió mal con el mensaje :('
+  } finally {
+    messageContainer.classList.add('fade-in')
+    loading(false)
+  }
+})
